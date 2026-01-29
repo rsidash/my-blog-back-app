a1cf17c643bb9955a8a80318f8f86c175ca20820
rsidash <roman.sidash@kaspi.kz>
2026-01-29 20:47:58 +0500

    add post image controller

---
+++ src/main/java/kz/rsidash/controller/PostImageController.java
@@ -0,0 +1,39 @@
+package kz.rsidash.controller;
+
+import jakarta.validation.constraints.NotNull;
+import kz.rsidash.Constants;
+import kz.rsidash.service.PostImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequiredArgsConstructor
+public class PostImageController {
+
+    private final PostImageService postImageService;
+
+    @PutMapping(value = Constants.POST_IMAGE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePostImage(
+            final @PathVariable @NotNull Long postId,
+            final @RequestParam("image") MultipartFile image
+    ) {
+        postImageService.upload(postId, image);
+    }
+
+    @GetMapping(Constants.POST_IMAGE)
+    public ResponseEntity<byte[]> getPostImage(final @PathVariable @NotNull Long postId) {
+        final var image = postImageService.getImage(postId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(image);
+    }
+
+}
